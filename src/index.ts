159d5031a4d1ca14412ab40648f5705c3c7dc6fc
muriz-dev <mhmdrizki.dev@gmail.com>
2026-07-12 14:09:26 +0700

    feat: setup cors, error handler and db middleware, register routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import { Hono } from 'hono'
+import { cors } from "hono/cors";
+import { dbMiddleware } from "./db/connection";
+import { errorHandler } from './middlewares/error-handler';
+import type { Env } from './types';
+import router from './routes';
 
-const app = new Hono()
+const app = new Hono<Env>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("*", (c, next) =>
+  cors({
+    origin: c.env.CORS_ORIGINS,
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })(c, next)
+);
+
+app.onError(errorHandler);
+
+app.use("*", dbMiddleware);
+
+app.route("/", router);
+
+app.get("/", (c) => {
+  return c.json({
+    success: true,
+    message: "Kompak API is running",
+  });
+});
 
 export default app
