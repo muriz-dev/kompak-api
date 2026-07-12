0fe99635b40592c490c1166ce8f8d39b061b5a6f
muriz-dev <mhmdrizki.dev@gmail.com>
2026-07-12 14:08:34 +0700

    feat: define modules router and documentation router

---
+++ src/routes.ts
@@ -0,0 +1,37 @@
+import { Hono } from "hono";
+import { openAPIRouteHandler } from "hono-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
+import type { Env } from "./types";
+
+import eventRouter from "./modules/events/event.route";
+import rewardRouter from "./modules/rewards/reward.route";
+
+const router = new Hono<Env>();
+
+// Modules Router
+router.route("/events", eventRouter);
+router.route("/rewards", rewardRouter);
+
+// Documentation Router
+router.get("/openapi", (c, _next) =>
+    openAPIRouteHandler(router, {
+        documentation: {
+            info: {
+                title: "Kompak API",
+                version: "1.0.0",
+                description: "Kompak Service",
+            },
+            servers: [{ url: c.env.API_BASE_URL, description: "Local Server" }],
+        },
+    })(c, _next)
+);
+
+router.get(
+    "/docs",
+    Scalar({
+        theme: "saturn",
+        url: "openapi",
+    }),
+);
+
+export default router;
